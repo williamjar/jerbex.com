7bfb124ee011dc4f26291bdf134a4f0e3e7ccfa3
williamjar <williaj@stud.ntnu.no>
2020-02-11 19:41:43 +0100

    a

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1><font size="100" color="white">ゲーミング !</font></h1>
+    <div className="App" >
+      <h1 onClick={() => window.open("https://www.youtube.com/watch?v=9bZkp7q19f0")}><font size="100" color="white">ゲーミング !</font></h1>
       
     </div>
   );
