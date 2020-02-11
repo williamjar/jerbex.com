34d67c39ff4d7e7f847161cdfb4657288cecf2ff
williamjar <williaj@stud.ntnu.no>
2020-02-11 19:37:56 +0100

    a

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img onClick={() => window.open("https://www.youtube.com/watch?v=9bZkp7q19f0")} src={"https://i.imgur.com/iqJ0pJo.jpg"}></img>
-
+      <h1><font size="100" color="white">ゲーミング !</font></h1>
       
     </div>
   );
