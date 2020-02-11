c044a569ef975b229045a68a66634a936f485e71
williamjar <williaj@stud.ntnu.no>
2020-02-11 19:10:04 +0100

    a

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# jerbex.com
+
+jerbex.com
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <img src={"https://i.imgur.com/iqJ0pJo.jpg"}></img>
+    </div>
+  );
+}
+
+export default App;
